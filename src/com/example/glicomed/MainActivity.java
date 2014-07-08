0a52b7a222b293f0c019d0c634fb9a2c82114a61
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-07-08 02:05:41 -0300

    MainActivity
    Button Exit

--- src/com/example/glicomed/MainActivity.java
+++ src/com/example/glicomed/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
 
         exit = (Button)findViewById(R.id.buttonExit);
         exit.setOnClickListener(btnExitListener);
